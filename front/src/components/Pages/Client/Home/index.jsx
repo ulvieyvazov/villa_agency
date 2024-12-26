d4185c5b0146fe945a5b1af9082f06365efb94a4
Ulvi <eyvazovulvi010101@gmail.com>
2024-12-26 19:39:29 +0400

    swiper js

--- front/src/components/Pages/Client/Home/index.jsx
+++ front/src/components/Pages/Client/Home/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./index.scss"
+// import SwiperSl from '../../../../SwiperSl.js';
 
 const Home = () => {
     return (
         <div id='home-container'>
             <div className='home-parent'>
                 <div className='home-carousel'>
-                    
                 </div>
             </div>
         </div>
